cdf5648d3aff815dbbc6631fafa3cf312fcefa73
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-02 06:01:26 +0200

    Add items when player joins the server

--- src/main/java/de/clientdns/smash/events/PlayerJoinListener.java
+++ src/main/java/de/clientdns/smash/events/PlayerJoinListener.java
@@ -1,19 +1,27 @@
 package de.clientdns.smash.events;
 
+import de.clientdns.smash.util.ItemStackUtil;
+import net.kyori.adventure.text.Component;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
 public class PlayerJoinListener implements Listener {
 
+    @SuppressWarnings("unused")
     @EventHandler
     void on(@NotNull PlayerJoinEvent event) {
         Player player = event.getPlayer();
+
+        player.setGameMode(GameMode.ADVENTURE);
 
         player.setHealth(20);
         player.setFoodLevel(20);
@@ -23,5 +31,18 @@ public class PlayerJoinListener implements Listener {
 
         Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_ATTACK_SPEED)).setBaseValue(1024);
         player.saveData();
+
+        player.getInventory().clear();
+
+        ItemStack characters = new ItemStackUtil().name("<gold>Charaktere</gold>").loreLines(" ", "<gray>Ändere deinen Charakter</gray>", " ").material(Material.CHEST).build();
+        player.getInventory().setItem(0, characters);
+
+        ItemStack maps = new ItemStackUtil().name("<gold>Maps</gold>").loreLines(" ", "<gray>Stimme für eine Map ab</gray>", " ").material(Material.MAP).build();
+        player.getInventory().setItem(4, maps);
+
+        ItemStack leave = new ItemStackUtil().name("<gold>Leave</gold>").loreLines("", "<gray>Verlasse das Spiel</gray>", " ").material(Material.SLIME_BALL).build();
+        player.getInventory().setItem(8, leave);
+
+        event.joinMessage(Component.empty());
     }
 }
