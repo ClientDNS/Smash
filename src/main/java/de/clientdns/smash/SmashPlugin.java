ee73e1b504d5d459c83d548ace607f52e108cc32
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-02 06:02:01 +0200

    Add some startup logic lol

--- src/main/java/de/clientdns/smash/SmashPlugin.java
+++ src/main/java/de/clientdns/smash/SmashPlugin.java
@@ -1,8 +1,9 @@
 package de.clientdns.smash;
 
-import de.clientdns.smash.events.EntityDamageListener;
-import de.clientdns.smash.events.FoodLevelChangeListener;
-import de.clientdns.smash.events.PlayerJoinListener;
+import de.clientdns.smash.character.CharacterCache;
+import de.clientdns.smash.events.*;
+import lombok.Getter;
+import org.bukkit.GameRule;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Duration;
@@ -10,18 +11,58 @@ import java.time.Instant;
 
 public class SmashPlugin extends JavaPlugin {
 
-    private Instant instant;
+    @Getter
+    private static SmashPlugin plugin;
 
-    @Override
-    public void onLoad() {
-        instant = Instant.now();
-    }
+    @Getter
+    private static CharacterCache characterCache;
 
     @Override
     public void onEnable() {
+
+        Instant instant = Instant.now();
+
+        plugin = this;
+        characterCache = new CharacterCache();
+
+        // new Character(1, "Mario", Ability.STAMP_ATTACK);
+
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+        getServer().getPluginManager().registerEvents(new BlockPlaceListener(), this);
         getServer().getPluginManager().registerEvents(new EntityDamageListener(), this);
         getServer().getPluginManager().registerEvents(new FoodLevelChangeListener(), this);
+        getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerDropItemListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerPickupItemListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
+
+        getServer().getWorlds().stream().findFirst().ifPresent(world -> {
+            world.setTime(1000);
+            world.setThundering(false);
+            world.setStorm(false);
+            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+            world.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+            world.setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
+            world.setGameRule(GameRule.DO_PATROL_SPAWNING, false);
+            world.setGameRule(GameRule.DO_TRADER_SPAWNING, false);
+            world.setGameRule(GameRule.DO_WARDEN_SPAWNING, false);
+            world.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+            world.setGameRule(GameRule.DO_MOB_LOOT, false);
+            world.setGameRule(GameRule.MOB_GRIEFING, false);
+            world.setGameRule(GameRule.NATURAL_REGENERATION, false);
+            world.setGameRule(GameRule.REDUCED_DEBUG_INFO, true);
+            world.setGameRule(GameRule.DO_ENTITY_DROPS, false);
+            world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, false);
+            world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, false);
+            world.setGameRule(GameRule.KEEP_INVENTORY, false);
+            world.setGameRule(GameRule.SHOW_DEATH_MESSAGES, false);
+            world.setGameRule(GameRule.SPECTATORS_GENERATE_CHUNKS, true);
+            world.setGameRule(GameRule.UNIVERSAL_ANGER, false);
+            world.setGameRule(GameRule.MAX_ENTITY_CRAMMING, 8);
+        });
+
         getLogger().info("SmashPlugin enabled in " + Duration.between(instant, Instant.now()).toMillis() + "ms");
     }
 }
