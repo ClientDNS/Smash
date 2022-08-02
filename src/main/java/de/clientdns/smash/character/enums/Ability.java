13da7ef3844bc457514386342aadad51d8df2473
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-02 05:59:46 +0200

    Add Abilities & Gadgets

---
+++ src/main/java/de/clientdns/smash/character/enums/Ability.java
@@ -0,0 +1,9 @@
+package de.clientdns.smash.character.enums;
+
+public enum Ability {
+
+    STAMP_ATTACK,
+    LESS_KNOCKBACK,
+    BOW_ATTACK,
+    KNOCKBACK_STRENGTH
+}
---
+++ src/main/java/de/clientdns/smash/character/enums/Gadget.java
@@ -0,0 +1,9 @@
+package de.clientdns.smash.character.enums;
+
+public enum Gadget {
+
+    MINIGUN,
+    HOT_STICK,
+    EXPLOSION_BOW,
+    MINIGUN_BOW
+}
