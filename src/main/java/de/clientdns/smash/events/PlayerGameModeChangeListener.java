db62e1a3323f0f06bfb08e2fcc9bd9bd1649a8a8
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-08 09:35:40 +0200

    Add listener

---
+++ src/main/java/de/clientdns/smash/events/PlayerGameModeChangeListener.java
@@ -0,0 +1,18 @@
+package de.clientdns.smash.events;
+
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerGameModeChangeListener implements Listener {
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    void on(@NotNull PlayerGameModeChangeEvent event) {
+        if (event.getNewGameMode().equals(GameMode.ADVENTURE) || event.getNewGameMode().equals(GameMode.SURVIVAL)) {
+            event.getPlayer().setAllowFlight(true);
+        }
+    }
+}
