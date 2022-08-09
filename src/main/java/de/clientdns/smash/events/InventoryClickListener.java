1be8c4228cebfeb3f5643c73750c4c60b73a12d4
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-09 11:25:49 +0200

    Player can now select characters

--- src/main/java/de/clientdns/smash/events/InventoryClickListener.java
+++ src/main/java/de/clientdns/smash/events/InventoryClickListener.java
@@ -1,5 +1,9 @@
 package de.clientdns.smash.events;
 
+import de.clientdns.smash.SmashPlugin;
+import de.clientdns.smash.character.Character;
+import de.clientdns.smash.character.CharacterCache;
+import de.clientdns.smash.gui.CharacterGui;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
@@ -8,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class InventoryClickListener implements Listener {
@@ -23,8 +28,47 @@ public class InventoryClickListener implements Listener {
                 return;
             }
 
-            player.sendMessage(Component.text("Not implemented yet!", NamedTextColor.RED));
-            event.setCancelled(true);
+            ItemStack item = event.getCurrentItem();
+            CharacterCache cache = SmashPlugin.getCharacterCache();
+            Character currentCharacter;
+
+            if (item.displayName().equals(CharacterGui.MARIO.displayName())) {
+                cache.remove(player);
+                cache.add(player, Character.MARIO);
+                currentCharacter = cache.get(player);
+                player.getInventory().close();
+                player.sendMessage(Component.text("Du hast den Charakter ", NamedTextColor.GRAY).append(currentCharacter.getName()).append(Component.text(" ausgewählt.", NamedTextColor.GRAY)));
+            } else if (item.displayName().equals(CharacterGui.DONKEY_KONG.displayName())) {
+                cache.remove(player);
+                cache.add(player, Character.DONKEY_KONG);
+                currentCharacter = cache.get(player);
+                player.getInventory().close();
+                player.sendMessage(Component.text("Du hast den Charakter ", NamedTextColor.GRAY).append(currentCharacter.getName()).append(Component.text(" ausgewählt.", NamedTextColor.GRAY)));
+            } else if (item.displayName().equals(CharacterGui.FLASH.displayName())) {
+                cache.remove(player);
+                cache.add(player, Character.FLASH);
+                currentCharacter = cache.get(player);
+                player.getInventory().close();
+                player.sendMessage(Component.text("Du hast den Charakter ", NamedTextColor.GRAY).append(currentCharacter.getName()).append(Component.text(" ausgewählt.", NamedTextColor.GRAY)));
+            } else if (item.displayName().equals(CharacterGui.PIKACHU.displayName())) {
+                cache.remove(player);
+                cache.add(player, Character.PIKACHU);
+                currentCharacter = cache.get(player);
+                player.getInventory().close();
+                player.sendMessage(Component.text("Du hast den Charakter ", NamedTextColor.GRAY).append(currentCharacter.getName()).append(Component.text(" ausgewählt.", NamedTextColor.GRAY)));
+            } else if (item.displayName().equals(CharacterGui.SUPERMAN.displayName())) {
+                cache.remove(player);
+                cache.add(player, Character.SUPERMAN);
+                currentCharacter = cache.get(player);
+                player.getInventory().close();
+                player.sendMessage(Component.text("Du hast den Charakter ", NamedTextColor.GRAY).append(currentCharacter.getName()).append(Component.text(" ausgewählt.", NamedTextColor.GRAY)));
+            } else if (item.displayName().equals(CharacterGui.LINK.displayName())) {
+                cache.remove(player);
+                cache.add(player, Character.LINK);
+                currentCharacter = cache.get(player);
+                player.getInventory().close();
+                player.sendMessage(Component.text("Du hast den Charakter ", NamedTextColor.GRAY).append(currentCharacter.getName()).append(Component.text(" ausgewählt.", NamedTextColor.GRAY)));
+            }
         } else {
             event.setCancelled(true);
         }
