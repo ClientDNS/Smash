4c747020b279241467e367d40fecaebae1c3da8d
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-09 11:31:51 +0200

    Update SmashPlugin.java

--- src/main/java/de/clientdns/smash/SmashPlugin.java
+++ src/main/java/de/clientdns/smash/SmashPlugin.java
@@ -1,9 +1,12 @@
 package de.clientdns.smash;
 
 import de.clientdns.smash.character.CharacterCache;
+import de.clientdns.smash.config.SmashConfig;
 import de.clientdns.smash.events.*;
 import lombok.Getter;
 import org.bukkit.GameRule;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Duration;
@@ -13,9 +16,11 @@ public class SmashPlugin extends JavaPlugin {
 
     @Getter
     private static SmashPlugin plugin;
-
+    @Getter
+    private static SmashConfig smashConfig;
     @Getter
     private static CharacterCache characterCache;
+    private int bukkitTask;
 
     @Override
     public void onEnable() {
@@ -23,21 +28,26 @@ public class SmashPlugin extends JavaPlugin {
         Instant instant = Instant.now();
 
         plugin = this;
+
+        // Initiating config file
+        smashConfig = new SmashConfig();
+
+        // Initiating character cache
         characterCache = new CharacterCache();
 
-        // new Character(1, "Mario", Ability.STAMP_ATTACK);
-
-        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
-        getServer().getPluginManager().registerEvents(new BlockPlaceListener(), this);
+        // Initiating event listeners
         getServer().getPluginManager().registerEvents(new EntityDamageListener(), this);
         getServer().getPluginManager().registerEvents(new FoodLevelChangeListener(), this);
         getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerDropItemListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerGameModeChangeListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerPickupItemListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
 
+        // Starting scheduler tasks
+        bukkitTask = getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> getServer().getWorlds().forEach(world -> world.getEntities().stream().filter(Item.class::isInstance).forEach(Entity::remove)), 0, 10);
+
+        // Initiating game rules
         getServer().getWorlds().stream().findFirst().ifPresent(world -> {
             world.setTime(1000);
             world.setThundering(false);
@@ -52,7 +62,7 @@ public class SmashPlugin extends JavaPlugin {
             world.setGameRule(GameRule.DO_MOB_LOOT, false);
             world.setGameRule(GameRule.MOB_GRIEFING, false);
             world.setGameRule(GameRule.NATURAL_REGENERATION, false);
-            world.setGameRule(GameRule.REDUCED_DEBUG_INFO, true);
+            world.setGameRule(GameRule.REDUCED_DEBUG_INFO, false);
             world.setGameRule(GameRule.DO_ENTITY_DROPS, false);
             world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, false);
             world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, false);
@@ -64,5 +74,13 @@ public class SmashPlugin extends JavaPlugin {
         });
 
         getLogger().info("SmashPlugin enabled in " + Duration.between(instant, Instant.now()).toMillis() + "ms");
+    }
+
+    @Override
+    public void onDisable() {
+        // Stopping scheduler tasks
+        if (getServer().getScheduler().isCurrentlyRunning(bukkitTask)) {
+            getServer().getScheduler().cancelTask(bukkitTask);
+        }
     }
 }
