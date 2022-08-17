b023de53d5447c834f490fee0cf8d243c168da7f
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-17 23:08:56 +0200

    Add Constants

---
+++ src/main/java/de/clientdns/smash/config/Constants.java
@@ -0,0 +1,36 @@
+package de.clientdns.smash.config;
+
+import de.clientdns.smash.SmashPlugin;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class Constants {
+
+    public static boolean setPlayerInAdventure() {
+        return SmashPlugin.getSmashConfig().getBoolean("config.set-player-in-adventure").orElse(false);
+    }
+
+    public static boolean disableJoinMessage() {
+        return SmashPlugin.getSmashConfig().getBoolean("config.disable-join-message").orElse(true);
+    }
+
+    public static boolean disableQuitMessage() {
+        return SmashPlugin.getSmashConfig().getBoolean("config.disable-quit-message").orElse(true);
+    }
+
+    @Contract(" -> new")
+    public static @NotNull Component prefix() {
+        return Component.text(SmashPlugin.getSmashConfig().getString("config.messages.prefix").orElse("§8[§6Smash§8] §r"));
+    }
+
+    @Contract(" -> new")
+    public static @NotNull Component joinMessage() {
+        return Component.text(SmashPlugin.getSmashConfig().getString("config.messages.join-message").orElse("§e%player% joined the game"));
+    }
+
+    @Contract(" -> new")
+    public static @NotNull Component quitMessage() {
+        return Component.text(SmashPlugin.getSmashConfig().getString("config.messages.quit-message").orElse("§e%player% left the game"));
+    }
+}
