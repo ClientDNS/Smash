5de2535a68ecc7e74188d5a8c2271a39e52ad088
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-20 22:29:45 +0200

    Work up config command a little bit

--- src/main/java/de/clientdns/smash/commands/ConfigCommand.java
+++ src/main/java/de/clientdns/smash/commands/ConfigCommand.java
@@ -1,6 +1,9 @@
 package de.clientdns.smash.commands;
 
 import de.clientdns.smash.SmashPlugin;
+import de.clientdns.smash.config.Constants;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,18 +17,18 @@ public class ConfigCommand implements CommandExecutor {
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("reload")) {
                     SmashPlugin.getPlugin().loadConfig();
-                    sender.sendMessage("§aConfig reloaded");
+                    sender.sendMessage(Constants.prefix().append(Component.text("Config reloaded.", NamedTextColor.GREEN)));
                     return true;
                 } else {
-                    sender.sendMessage("§cInvalid argument");
+                    sender.sendMessage(Constants.prefix().append(Component.text("Invalid argument: " + args[0], NamedTextColor.RED)));
                     return false;
                 }
             } else {
-                sender.sendMessage("§c/config reload");
+                sender.sendMessage(Constants.prefix().append(Component.text("§c/config §8<§creload§8>")));
                 return false;
             }
         } else {
-            sender.sendMessage("§cYou don't have permission to do this!");
+            sender.sendMessage(Component.text("You don't have permission to do this!", NamedTextColor.RED));
         }
         return false;
     }
