fc29137e408c0acc77e6b74de373275ef25f6812
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-10-13 22:38:33 +0200

    Update Gadget class

--- src/main/java/de/clientdns/smash/character/enums/Gadget.java
+++ src/main/java/de/clientdns/smash/character/enums/Gadget.java
@@ -2,8 +2,17 @@ package de.clientdns.smash.character.enums;
 
 public enum Gadget {
 
-    MINIGUN,
-    HOT_STICK,
-    EXPLOSION_BOW,
-    MINIGUN_BOW;
+    MINIGUN("Minigun"),
+    HOT_STICK("Hot Stick"),
+    EXPLOSION_BOW("Explosion Bow");
+
+    private final String friendlyName;
+
+    Gadget(String friendlyName) {
+        this.friendlyName = friendlyName;
+    }
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
 }
