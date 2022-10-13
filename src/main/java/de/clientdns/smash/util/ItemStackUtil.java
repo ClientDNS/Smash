f7d909dd6b70ee3ffc02375d530ea707c134de43
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-10-13 19:06:19 +0200

    Expand ItemStackUtil a little bit

--- src/main/java/de/clientdns/smash/util/ItemStackUtil.java
+++ src/main/java/de/clientdns/smash/util/ItemStackUtil.java
@@ -1,76 +1,111 @@
 package de.clientdns.smash.util;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import static net.kyori.adventure.text.Component.empty;
 
 @SuppressWarnings("unused")
 public class ItemStackUtil {
 
-    private final MiniMessage miniMessage = MiniMessage.miniMessage();
-    private final ItemStack itemStack;
-    private final ItemMeta itemMeta;
+    private ItemStack itemStack;
+    private ItemMeta itemMeta;
 
+    /**
+     * Creates a new ItemStackUtil instance with the given {@link Material} and default amount of 1.
+     *
+     * @param material The material of the item.
+     */
     public ItemStackUtil(Material material) {
+        new ItemStackUtil(material, 1);
+    }
+
+    /**
+     * Creates a new ItemStackUtil with the given {@link Material} and amount.
+     *
+     * @param material The material of the item.
+     * @param amount   The amount of the item.
+     */
+    public ItemStackUtil(Material material, int amount) {
+        new ItemStackUtil(material, amount, empty());
+    }
+
+    public ItemStackUtil(Material material, int amount, Component displayName) {
         if (material == null) {
             throw new NullPointerException("Material must not be null.");
         }
-        itemStack = new ItemStack(material);
-        itemMeta = itemStack.getItemMeta();
-    }
-
-    public ItemStackUtil(Material material, short amount) {
         itemStack = new ItemStack(material, amount);
         itemMeta = itemStack.getItemMeta();
+        itemMeta.displayName(displayName);
     }
 
-    public ItemStackUtil amount(short amount) {
-        itemStack.setAmount(amount);
-        return this;
-    }
-
+    /**
+     * Sets the display name of the item.
+     *
+     * @param name The name of the item.
+     * @return The ItemStackUtil instance.
+     */
     public ItemStackUtil name(@NotNull Component name) {
         itemMeta.displayName(name);
         return this;
     }
 
+    /**
+     * Sets the unbreakable state of the item.
+     *
+     * @param unbreakable The unbreakable state.
+     * @return The ItemStackUtil instance.
+     */
     public ItemStackUtil unbreakable(boolean unbreakable) {
         itemMeta.setUnbreakable(unbreakable);
         return this;
     }
 
-    public ItemStackUtil loreLines(@NotNull String @NotNull ... lines) {
+    /**
+     * Sets the lore of the item.
+     *
+     * @param lines The lines of the lore.
+     * @return The ItemStackUtil instance.
+     */
+    public ItemStackUtil loreLines(@NotNull Component @NotNull ... lines) {
         if (lines.length == 0) {
             throw new IllegalArgumentException("Lore lines must not be empty.");
         }
-        List<Component> lore = Arrays.stream(lines).toList().stream().map(miniMessage::deserialize).collect(Collectors.toList());
-        itemMeta.lore(lore);
+        itemMeta.lore(List.of(lines));
         return this;
     }
 
+    /**
+     * Sets the enchantments of the item.
+     *
+     * @param enchantments The enchantments.
+     * @return The ItemStackUtil instance.
+     */
     public ItemStackUtil enchants(@NotNull Map<Enchantment, Integer> enchantments) {
-        if (enchantments.isEmpty()) {
-            throw new IllegalArgumentException("Enchantments must not be empty.");
+        for (Enchantment enchantment : enchantments.keySet()) {
+            itemMeta.addEnchant(enchantment, enchantments.get(enchantment), true); // Ignore level limit
         }
-        enchantments.forEach((enchantment, level) -> itemMeta.addEnchant(enchantment, level, true));
         return this;
     }
 
+    /**
+     * Builds the item.
+     *
+     * @return The item as {@link ItemStack}.
+     */
     public ItemStack build() {
         if (itemStack == null) {
-            throw new NullPointerException("ItemStack must not be null.");
+            return null;
         }
         if (itemMeta == null) {
-            throw new NullPointerException("ItemMeta must not be null.");
+            return null;
         }
         itemStack.setItemMeta(itemMeta);
         return itemStack;
