b4bcb8972fe0da292037a1ce1b08865d7d32e5bf
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-10-23 23:26:07 +0200

    Add some explanations

--- src/main/java/de/clientdns/smash/config/Value.java
+++ src/main/java/de/clientdns/smash/config/Value.java
@@ -1,16 +1,28 @@
 package de.clientdns.smash.config;
 
 import de.clientdns.smash.SmashPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public class Value<V> {
+public class Value<V> { // V = Expected value type (e.g. String, Integer, Boolean, etc.)
 
+    private final String path;
     private final V value;
 
-    public Value(String path) {
-        value = SmashPlugin.getPlugin().getSmashConfig().get(path);
+    /**
+     * Retrieves the value from the config.
+     * If the value is not of the expected value type, an exception {@link ClassCastException} is thrown.
+     *
+     * @param path The path to the value
+     */
+    public Value(String path) throws NullPointerException {
+        this.path = path;
+        this.value = SmashPlugin.getPlugin().getSmashConfig().get(path);
     }
 
-    public V get() {
-        return value;
+    public @NotNull V get() throws NullPointerException {
+        if (this.value == null) {
+            throw new NullPointerException("Value (path: " + this.path + ") is null.");
+        }
+        return this.value;
     }
 }
