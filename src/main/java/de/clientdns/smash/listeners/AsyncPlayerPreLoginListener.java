3c8743f9c3aa304ff7ee3ca26fdc5eedbaae25ec
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-10-26 05:09:30 +0200

    Use import of pre login event result

--- src/main/java/de/clientdns/smash/listeners/AsyncPlayerPreLoginListener.java
+++ src/main/java/de/clientdns/smash/listeners/AsyncPlayerPreLoginListener.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.jetbrains.annotations.NotNull;
 
 import static net.kyori.adventure.text.Component.text;
@@ -16,7 +17,7 @@ public class AsyncPlayerPreLoginListener implements Listener {
         int online = Bukkit.getOnlinePlayers().size();
         int maxPlayers = Bukkit.getMaxPlayers();
         if (online >= maxPlayers) {
-            event.disallow(org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result.KICK_FULL, text("Der Server ist voll!", NamedTextColor.RED));
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_FULL, text("Der Server ist voll!", NamedTextColor.RED));
         }
     }
 }
