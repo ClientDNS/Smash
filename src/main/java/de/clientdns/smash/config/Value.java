365584a9d78de27f14d3642d31630ebc23598317
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-10-26 05:09:09 +0200

    Optimize Value generic class

--- src/main/java/de/clientdns/smash/config/Value.java
+++ src/main/java/de/clientdns/smash/config/Value.java
@@ -5,23 +5,32 @@ import org.jetbrains.annotations.NotNull;
 
 public class Value<V> { // V = Expected value type (e.g. String, Integer, Boolean, etc.)
 
-    private final String path;
     private final V value;
 
     /**
      * Retrieves the value from the config.
-     * If the value is not of the expected value type, an exception {@link ClassCastException} is thrown.
      *
      * @param path The path to the value
      */
     public Value(String path) throws NullPointerException {
-        this.path = path;
-        this.value = SmashPlugin.getPlugin().getSmashConfig().get(path);
+        if (!isNull(path)) {
+            this.value = SmashPlugin.getPlugin().getSmashConfig().get(path);
+        } else {
+            throw new NullPointerException("Value of " + path + " is null");
+        }
     }
 
-    public @NotNull V get() throws NullPointerException {
-        if (this.value == null) {
-            throw new NullPointerException("Value (path: " + this.path + ") is null.");
+    private boolean isNull(String path) {
+        return SmashPlugin.getPlugin().getSmashConfig().get(path) == null;
+    }
+
+    @NotNull
+    public V get() {
+        if (this.value.toString().isBlank()) {
+            throw new IllegalStateException("Value is blank");
+        }
+        if (this.value.toString().isEmpty()) {
+            throw new IllegalStateException("Value is empty");
         }
         return this.value;
     }
