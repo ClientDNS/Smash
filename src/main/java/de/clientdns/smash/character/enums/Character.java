f5e92a67d45cae0ce1dc51576b849f5315a395f7
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-11-05 18:37:58 +0100

    Use static import in Character class

--- src/main/java/de/clientdns/smash/character/enums/Character.java
+++ src/main/java/de/clientdns/smash/character/enums/Character.java
@@ -1,21 +1,21 @@
 package de.clientdns.smash.character.enums;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.security.SecureRandom;
 import java.util.List;
 
 import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 public enum Character {
 
-    MARIO(0, text("Mario", NamedTextColor.RED), List.of(Ability.STAMP_ATTACK)),
-    DONKEY_KONG(1, text("Donkey Kong", NamedTextColor.DARK_RED), List.of(Ability.STAMP_ATTACK)),
-    FLASH(2, text("Flash", NamedTextColor.RED), List.of(Ability.SPEED)),
-    PIKACHU(3, text("Pikachu", NamedTextColor.YELLOW), List.of(Ability.STAMP_ATTACK)),
-    SUPERMAN(4, text("Superman", NamedTextColor.RED), List.of(Ability.STAMP_ATTACK)),
-    LINK(5, text("Link", NamedTextColor.DARK_GREEN), List.of(Ability.SPEED_AND_JUMPBOOST));
+    MARIO(0, text("Mario", RED), List.of(Ability.STAMP_ATTACK)),
+    DONKEY_KONG(1, text("Donkey Kong", DARK_RED), List.of(Ability.STAMP_ATTACK)),
+    FLASH(2, text("Flash", RED), List.of(Ability.SPEED)),
+    PIKACHU(3, text("Pikachu", YELLOW), List.of(Ability.STAMP_ATTACK)),
+    SUPERMAN(4, text("Superman", RED), List.of(Ability.STAMP_ATTACK)),
+    LINK(5, text("Link", DARK_GREEN), List.of(Ability.SPEED_AND_JUMPBOOST));
 
     private final int id;
     private final Component name;
