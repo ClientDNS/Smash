22762d850a7efe2378174d9b600874d697d98548
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-12-14 23:06:58 +0100

    Copied new methods for the FormatUtil :)

--- src/main/java/de/clientdns/smash/util/FormatUtil.java
+++ src/main/java/de/clientdns/smash/util/FormatUtil.java
@@ -1,14 +1,30 @@
 package de.clientdns.smash.util;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class FormatUtil {
 
-    public static @NotNull String formatSeconds(long seconds) {
-        if (seconds <= 0) {
-            return "0s";
-        }
+    private static final String[] SIZE_UNITS = {"bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
 
+    @Contract(pure = true)
+    public static @NotNull String percent(double value, double max) {
+        double percent = (value * 100D) / max;
+        return (int) percent + "%";
+    }
+
+    public static @NotNull String formatBytes(long bytes) {
+        if (bytes <= 0)
+            return "0 bytes";
+        int sizeIndex = (int) (Math.log(bytes) / Math.log(1024));
+        return String.format("%.1f", bytes / Math.pow(1024, sizeIndex)) + " " + SIZE_UNITS[sizeIndex];
+    }
+
+    public static @NotNull String formatSeconds(long seconds) {
+        if (seconds <= 0)
+            return "0s";
+
+        long milliSecond = seconds / 1000;
         long second = seconds;
         long minute = second / 60;
         long hour = minute / 60;
@@ -16,18 +32,17 @@ public class FormatUtil {
         second = second % 60;
 
         StringBuilder sb = new StringBuilder();
-        if (day != 0) {
+        if (day != 0)
             sb.append(day).append("d ");
-        }
-        if (hour != 0) {
+        if (hour != 0)
             sb.append(hour).append("h ");
-        }
-        if (minute != 0) {
+        if (minute != 0)
             sb.append(minute).append("m ");
-        }
-        if (second != 0) {
+        if (second != 0)
             sb.append(second).append("s ");
-        }
+        if (milliSecond != 0)
+            sb.append(milliSecond).append("ms ");
+
 
         return sb.toString().trim();
     }
