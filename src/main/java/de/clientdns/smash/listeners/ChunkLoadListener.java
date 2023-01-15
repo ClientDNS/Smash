85d340dd6f30b35ffe799078c13c5ade0c165517
Ian <55300372+ClientDNS@users.noreply.github.com>
2023-01-15 02:10:39 +0100

    Do not count entities killed in a chunk

--- src/main/java/de/clientdns/smash/listeners/ChunkLoadListener.java
+++ src/main/java/de/clientdns/smash/listeners/ChunkLoadListener.java
@@ -1,8 +1,7 @@
 package de.clientdns.smash.listeners;
 
-import de.clientdns.smash.SmashPlugin;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
@@ -12,15 +11,12 @@ public class ChunkLoadListener implements Listener {
 
     @EventHandler
     void on(@NotNull ChunkLoadEvent event) {
-        int count = 0;
-        for (Entity entity : event.getWorld().getEntities()) {
+        for (LivingEntity entity : event.getWorld().getLivingEntities()) {
             if (entity.getType().equals(EntityType.DROPPED_ITEM)) {
-                entity.remove();
-                count++;
+                if (!entity.isDead()) {
+                    entity.remove();
+                }
             }
-        }
-        if (count > 1) {
-            SmashPlugin.getPlugin().getLogger().info("Killed '" + count + "' entities in " + event.getChunk());
         }
     }
 }
