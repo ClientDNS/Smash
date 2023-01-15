368dd8163808c92e2c96133fdd11f340d4af86ce
Ian <55300372+ClientDNS@users.noreply.github.com>
2023-01-15 02:11:15 +0100

    Rename config constant

--- src/main/java/de/clientdns/smash/api/config/MiniMsg.java
+++ src/main/java/de/clientdns/smash/api/config/MiniMsg.java
@@ -2,19 +2,17 @@ package de.clientdns.smash.api.config;
 
 import de.clientdns.smash.SmashPlugin;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.util.RGBLike;
 import org.jetbrains.annotations.NotNull;
 
 public class MiniMsg {
 
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
-    private static final SmashConfig smashConfig = SmashPlugin.getPlugin().getSmashConfig();
+    private static final SmashConfig SMASH_CONFIG = SmashPlugin.getPlugin().getSmashConfig();
 
     public static Component mini(String path) {
-        return MINI_MESSAGE.deserializeOrNull(String.valueOf(smashConfig.getString(path)));
+        return MINI_MESSAGE.deserializeOrNull(String.valueOf(SMASH_CONFIG.getString(path)));
     }
 
     public static <R> @NotNull Component plain(R message) {
