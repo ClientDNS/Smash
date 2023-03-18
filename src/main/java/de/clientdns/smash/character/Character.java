c17ad15994c71ee859ccbaa177788d2cb711ea7b
Ian <55300372+ClientDNS@users.noreply.github.com>
2023-03-18 19:08:33 +0100

    Add id to Character enum

--- src/main/java/de/clientdns/smash/character/Character.java
+++ src/main/java/de/clientdns/smash/character/Character.java
@@ -9,12 +9,12 @@ import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 public enum Character {
 
-    MARIO(new CharacterData(MiniMsg.plain("Mario", RED), "", List.of(Ability.STAMP_ATTACK))), // Empty string
-    DONKEY_KONG(new CharacterData(MiniMsg.plain("Donkey Kong", DARK_RED), "", List.of(Ability.STAMP_ATTACK))),
-    FLASH(new CharacterData(MiniMsg.plain("Flash", RED), "https://minotar.net/skin/Shlappeh", List.of(Ability.SPEED))),
-    PIKACHU(new CharacterData(MiniMsg.plain("Pikachu", YELLOW), "", List.of(Ability.STAMP_ATTACK))),
-    SUPERMAN(new CharacterData(MiniMsg.plain("Superman", RED), "", List.of(Ability.STAMP_ATTACK))),
-    LINK(new CharacterData(MiniMsg.plain("Link", DARK_GREEN), "", List.of(Ability.JUMPBOOST, Ability.SPEED)));
+    MARIO(new CharacterData(0, MiniMsg.plain("Mario", RED), "", List.of(Ability.STAMP_ATTACK))), // Empty string
+    DONKEY_KONG(new CharacterData(1, MiniMsg.plain("Donkey Kong", DARK_RED), "", List.of(Ability.STAMP_ATTACK))),
+    FLASH(new CharacterData(2, MiniMsg.plain("Flash", RED), "https://minotar.net/skin/Shlappeh", List.of(Ability.SPEED))),
+    PIKACHU(new CharacterData(3, MiniMsg.plain("Pikachu", YELLOW), "", List.of(Ability.STAMP_ATTACK))),
+    SUPERMAN(new CharacterData(4, MiniMsg.plain("Superman", RED), "", List.of(Ability.STAMP_ATTACK))),
+    LINK(new CharacterData(5, MiniMsg.plain("Link", DARK_GREEN), "", List.of(Ability.JUMPBOOST, Ability.SPEED)));
 
     private final CharacterData data;
 
