b43576b0b9bb71de83607b002af61eb3cc7e5761
Ian <55300372+ClientDNS@users.noreply.github.com>
2023-03-18 19:08:08 +0100

    Convert CharacterData class to record

--- src/main/java/de/clientdns/smash/character/CharacterData.java
+++ src/main/java/de/clientdns/smash/character/CharacterData.java
@@ -4,27 +4,5 @@ import net.kyori.adventure.text.Component;
 
 import java.util.List;
 
-public class CharacterData {
-
-    private final Component name;
-    private final String skinUrl;
-    private final List<Ability> abilities;
-
-    public CharacterData(Component name, String skinUrl, List<Ability> abilities) {
-        this.name = name;
-        this.skinUrl = skinUrl;
-        this.abilities = abilities;
-    }
-
-    public Component getName() {
-        return name;
-    }
-
-    public String getSkinUrl() {
-        return skinUrl;
-    }
-
-    public List<Ability> getAbilities() {
-        return abilities;
-    }
+public record CharacterData(int id, Component name, String skinUrl, List<Ability> abilities) {
 }
