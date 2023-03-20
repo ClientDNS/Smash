b71cbc94234c4821d241a3086e6477b6754382b5
Ian <55300372+ClientDNS@users.noreply.github.com>
2023-03-20 16:29:34 +0100

    Convert Vote record back to class

--- src/main/java/de/clientdns/smash/voting/Vote.java
+++ src/main/java/de/clientdns/smash/voting/Vote.java
@@ -1,6 +1,23 @@
 package de.clientdns.smash.voting;
 
 import de.clientdns.smash.map.Map;
+import org.bukkit.entity.Player;
 
-public record Vote(Map map) {
+public class Vote {
+
+    private final Player player;
+    private final Map map;
+
+    public Vote(Player player, Map map) {
+        this.player = player;
+        this.map = map;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Map getMap() {
+        return map;
+    }
 }
