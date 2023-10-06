3ffab3c4b269b86971d5860958ca954391644f2d
Ian <55300372+ClientDNS@users.noreply.github.com>
2023-10-06 23:39:58 +0200

    Update paper-api to 1.20.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
     compileOnly("com.google.code.gson:gson:2.10.1")
     //implementation("de.chojo.sadu:sadu:1.2.0")
 }
